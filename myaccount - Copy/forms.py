9dc6d1ef426cbfcae69cfab3df979b039acc7d2a
vaibhavgirase635 <vaibhavrajput635@gmail.com>
2022-12-17 17:38:56 +0530

    second commit

--- myaccount - Copy/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from django.forms import NumberInput
-
-class BVNForm(forms.Form): 
-    bvn = forms.CharField(widget=NumberInput(attrs={'class':'form-control', 'placeholder':'Your BVN', 'required':'required'}))
